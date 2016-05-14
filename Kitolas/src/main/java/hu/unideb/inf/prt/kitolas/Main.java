601387ea9acc9556f029e9c71ad52a4fab42aeab
lac15 <laszlo.erdohegyi@gmail.com>
2016-05-14 16:35:34 +0200

    elso commit, alapok

---
+++ Kitolas/src/main/java/hu/unideb/inf/prt/kitolas/Main.java
@@ -0,0 +1,19 @@
+package hu.unideb.inf.prt.kitolas;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
